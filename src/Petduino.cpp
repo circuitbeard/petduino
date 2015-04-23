86d49874477ecd1d4e30e1f111c42dc985e201c0
Matt Brailsford <me@mattbrailsford.com>
2015-04-23 14:11:20 +0100

    Update Petduino.cpp

--- src/Petduino.cpp
+++ src/Petduino.cpp
@@ -1,1 +1,30 @@
+#include "Petduino.h"
 
+Petduino *thisPetduino = NULL;
+
+Petduino::Petduino() {
+}
+
+// Setup inputs, outputs, timers, etc. Call this from main arduino setup() method!!
+void Gamer::setup(){
+  ::thisPetduino = this;
+  
+  // Setup outputs
+  
+  // Setup inputs
+  
+}
+
+// Private -------------------------------------------
+
+// Public --------------------------------------------
+
+// Set the value of the Petduino LED
+void Petduino::setLED(bool value) {
+  digitalWrite(LED, value);
+}
+
+// Toggle the Petduino LED
+void Petduino::toggleLED() {
+  digitalWrite(LED, !digitalRead(LED));
+}

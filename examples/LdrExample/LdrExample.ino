31a8f941bba5a035cb624cae614519214d4a8947
Matt Brailsford <me@mattbrailsford.com>
2015-04-25 10:45:30 +0100

    Added LDR example

--- examples/LdrExample/LdrExample.ino
+++ examples/LdrExample/LdrExample.ino
@@ -27,9 +27,91 @@
 #include <LedControl.h>
 #include <Petduino.h>
 
-// Awake
-// Snoozy
-// Asleep
+#define FRAMECOUNT 8
+byte frames[FRAMECOUNT][8] = {
+  {
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B10000000
+  },
+  {
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B01000000,
+    B11000000
+  },
+  {
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00100000,
+    B01100000,
+    B11100000
+  },
+  {
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00010000,
+    B00110000,
+    B01110000,
+    B11110000
+  },
+  {
+    B00000000,
+    B00000000,
+    B00000000,
+    B00001000,
+    B00011000,
+    B00111000,
+    B01111000,
+    B11111000
+  },
+  {
+    B00000000,
+    B00000000,
+    B00000100,
+    B00001100,
+    B00011100,
+    B00111100,
+    B01111100,
+    B11111100
+  },
+  {
+    B00000000,
+    B00000010,
+    B00000110,
+    B00001110,
+    B00011110,
+    B00111110,
+    B01111110,
+    B11111110
+  },
+  {
+    B00000001,
+    B00000011,
+    B00000111,
+    B00001111,
+    B00011111,
+    B00111111,
+    B01111111,
+    B11111111
+  }
+};
+
+unsigned int currentFrame;
 
 Petduino pet = Petduino();
 
@@ -44,5 +126,14 @@ void loop() {
   
   // Call pet loop
   pet.loop();
+  
+  // Map reading to a frame
+  currentFrame = map(pet.getLightLevel(), 0, 1023, 0, FRAMECOUNT);
+  
+  // Draw the frame
+  pet.drawImage(frames[currentFrame]);
+  
+  // Wait a split second and repeat
+  delay(200);
 
 }

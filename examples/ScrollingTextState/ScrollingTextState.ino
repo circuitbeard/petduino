c5a25b890155229deef6ecdbe56424db696b6ec0
Matt Brailsford <me@mattbrailsford.com>
2015-10-26 20:13:21 +0000

    Added state based scrolling text example

--- examples/ScrollingTextState/ScrollingTextState.ino
+++ examples/ScrollingTextState/ScrollingTextState.ino
@@ -1,5 +1,5 @@
 /*
- *    ScrollingText.ino - Scrolling text example using the Petduino library
+ *    ScrollingTextState.ino - Scrolling text example using the Petduino library
  *    Copyright (c) 2015 Circuitbeard
  *
  *    Permission is hereby granted, free of charge, to any person
@@ -27,7 +27,7 @@
 #include <avr/pgmspace.h> 
 #include <LedControl.h>
 #include <Petduino.h>
-#include "font_thin.h"
+#include "font_bold.h"
 
 #define CHAR_HEIGHT 8
 #define BUFFER_SIZE CHAR_HEIGHT*2
@@ -38,12 +38,25 @@
 const unsigned char scrollText[] PROGMEM = {"HELLO WORLD #CIRCUITBEARD 2015  \0"}; // Make sure you have enough space on the end to fill the buffer and ensure all chars scroll across screen
 unsigned long charBuffer[BUFFER_SIZE] = {0};
 
+unsigned int charCounter = 0;
+unsigned int nextChar = 0;
+
+unsigned int scrollLen = 0;
+unsigned int scrollCount = 0;
+
+#define INIT_STATE 0
+#define LOAD_CHAR_STATE 1
+#define LOOP_STATE 2
+
 Petduino pet = Petduino();
 
 void setup() {
   
   // Setup Petduino
   pet.begin();
+
+  // Set initial state
+  pet.setState(INIT_STATE);
 
 }
 
@@ -52,41 +65,82 @@ void loop() {
   // Call pet loop
   pet.update();
 
-  // Scroll message
-  scrollMessage(scrollText);
+  // Update based on state
+  switch(pet.getState()) {
 
-}
+    case INIT_STATE:
 
-// Scroll Message
-void scrollMessage(const unsigned char *messageString) {
-    int counter = 0;
-    int myChar=0;
-    do {
-        // read back a char
-        myChar =  pgm_read_byte_near(messageString + counter);
-        if (myChar != 0){
-            loadCharIntoBuffer(myChar);
-        }
-        counter++;
-    }
-    while (myChar != 0);
+      // Reset vars
+      charCounter = 0;
+      nextChar = 0;
+
+      // Start loading text into buffer
+      pet.setState(LOAD_CHAR_STATE);
+      break;
+
+    case LOAD_CHAR_STATE:
+
+      // Grab the next character
+      nextChar =  pgm_read_byte_near(scrollText + charCounter);
+
+      if (nextChar != 0){
+
+        // Load char into buffer and reset scroll vars
+        scrollLen = loadCharIntoBuffer(nextChar);
+        scrollCount = 0;
+
+        // Increment char counter
+        charCounter++;
+
+        // Draw current buffer
+        pet.setState(LOOP_STATE);
+
+      } else {
+
+        // Finished so start again
+        pet.setState(INIT_STATE);
+
+      }
+
+      break;
+    case LOOP_STATE:
+
+      if(scrollCount < scrollLen){
+        
+        // Shift the buffer along 1px
+        rotateBuffer();
+        
+        // Draw the current buffer
+        drawBuffer();
+        
+        // Increment the scroll counter
+        scrollCount++;
+        
+        // Loop
+        pet.setNextState(LOOP_STATE, SCROLL_SPEED);
+        
+      } else {
+        
+        // Load next char
+        pet.setState(LOAD_CHAR_STATE);
+        
+      }
+
+      break;
+  }
+
 }
 
 // Load character into buffer
-void loadCharIntoBuffer(int ascii){
+unsigned int loadCharIntoBuffer(unsigned int ascii){
   if (ascii >= MIN_CHAR && ascii <= MAX_CHAR){ 
-      for (int a=0; a<CHAR_HEIGHT; a++) {                      
-          unsigned long c = pgm_read_byte_near(font + ((ascii - 0x20) * (CHAR_HEIGHT+1)) + a);     // Index into character table to get row data
-          unsigned long x = charBuffer[a*2];     // Load current buffer value
-          x = x | c;                             // OR the new character onto end of current
-          charBuffer[a*2] = x;                   // Store back in buffer
-      }
-      byte count = pgm_read_byte_near(font +((ascii - 0x20) * (CHAR_HEIGHT+1)) + CHAR_HEIGHT);     // Index into character table for kerning data
-      for (byte x=0; x<count;x++){
-          rotateBuffer();
-          drawBuffer();
-          delay(SCROLL_SPEED);
-      }
+    for (int a=0; a<CHAR_HEIGHT; a++) {                      
+        unsigned long c = pgm_read_byte_near(font + ((ascii - 0x20) * (CHAR_HEIGHT+1)) + a);     // Index into character table to get row data
+        charBuffer[a*2] = charBuffer[a*2] | c; // Bitwise OR character onto end
+    }
+    return pgm_read_byte_near(font + ((ascii - 0x20) * (CHAR_HEIGHT+1)) + CHAR_HEIGHT);     // Index into character table for kerning data
+  } else {
+    return 0;
   }
 }
 
@@ -96,7 +150,7 @@ void rotateBuffer(){
     unsigned long x = charBuffer[a*2];     // Get low buffer entry
     byte b = bitRead(x,31);                // Copy high order bit that gets lost in rotation
     x = x<<1;                              // Rotate left one bit
-    charBuffer[a*2] = x;                   // Store new low buffer
+    charBuffer[a*2] = x;                   // Store new low buffer  
     x = charBuffer[a*2+1];                 // Get high buffer entry
     x = x<<1;                              // Rotate left one bit
     bitWrite(x,0,b);                       // Store saved bit
